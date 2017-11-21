9d7548388a1fea09eb08fd2b3a9012a6aeb2c2a3
Andrew Kang <andrew716@gmail.com>
2017-11-21 04:00:57 -0800

    ...

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-commit=`git show --summary | grep commit | awk '{print $2}'`
-read -r -p "Are you sure to update PRODUCTION with $commit? [y/n] " response
+commitId=$1
+echo $commitId
 
+if [[ -z "$commitId" ]]; then
+  commitId=`git show --summary | grep commit | awk '{print $2}'`
+fi
+read -r -p "Are you sure to deply revision $commitId? [y/n] " response
+
+exit
 if [[ $response =~ ^[Yy]$ ]]; then
   aws deploy create-deployment \
     --application-name TestApp \
     --deployment-config-name CodeDeployDefault.OneAtATime \
     --deployment-group-name TestAppGroup \
-    --github-location repository=chamchisand/codedeploy,commitId=$commit
+    --github-location repository=chamchisand/codedeploy,commitId=$commitId
 fi
--- scripts/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 8.9.1
-npm i pm2@latest -g
