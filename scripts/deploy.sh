0136c0b5b1887f2d7f6229865c62f9c1ae703d67
Andrew Kang <andrew716@gmail.com>
2017-11-21 00:57:38 -0800

    add deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+commit=`git show --summary | grep commit | awk '{print $2}'`
+read -r -p "Are you sure to update PRODUCTION with $commit? [y/n] " response
+
+if [[ $response =~ ^[Yy]$ ]]; then
+  aws deploy create-deployment \
+    --application-name TestApp \
+    --deployment-config-name CodeDeployDefault.OneAtATime \
+    --deployment-group-name www \
+    --github-location repository=chamchisand/codedeploy,commitId=$commit
+fi
